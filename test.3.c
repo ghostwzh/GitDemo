3299fa364d5e8c44dbb2df76caa28d47b1b73f58
ghostwzh <ghostwzh@tom.com>
2023-03-15 15:29:13 +0800

    测试命令行提交

---
+++ test.3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    switch(a)
+    {
+     case 1:
+         printf("星期1");
+     break;
+     case 2:
+         printf("星期2");
+     break;
+     case 3:
+         printf("星期3");
+     break;
+     case 4:
+         printf("星期4");
+     break;
+     case 5:
+         printf("星期5");
+     break;
+     case 6:
+         printf("星期6");
+     break;
+     case 7:
+         printf("星期天");
+     break;
+
+    }
+ return 0;
+}

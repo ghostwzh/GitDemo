eb2d507bdc55aaa972b50ec587a2d5d5b8626e7f
ghostwzh <ghostwzh@tom.com>
2023-03-21 22:13:26 +0800

    the c code

---
+++ test2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+/*{
+    //double d = 3.14;
+    //double* pd = &d;
+    //int a = 10;
+    //printf("%p\n",&a);
+    //int*p = &a;
+    //printf("%p\n",p);//p是一个指针变量
+    //*p = 20;//*-解引用操作符/间接访问操作符0
+    //printf("%d\n",sizeof(pd));
+    int a,b,c,d,e;
+    scanf("%d%d%d",&a,&b,&e);
+    c = a>b?a:b;
+    d = c>e?c:e;
+    printf("%d",d);
+    return 0;
+}
+{
+    int a;
+    for(a = 0;a++;a<100)
+    
+}*/
+
+
+
+{
+    printf("你好\n");
+    return 0;
+}
